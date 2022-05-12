a9227adca172dade1752b10c2575d71aec30cee4
yongjae7088 <yongyonglv@naver.com>
2022-05-12 13:16:35 +0900

    return값 변경

--- newsbot.py
+++ newsbot.py
@@ -39,8 +39,19 @@ def newsflash_main():
     list_href = newsflash_href(soup)
     list_title = newsflash_title(soup)
     list_company = newsflash_company(soup)
- 
-    return list_href, list_title, list_company
+
+    result = list_title[0] + " - " + list_company[0] + "\n" + list_href[0] + "\n\n" + list_title[1] + " - " + list_company[1] + "\n" + list_href[1] + '''\n
+''' + list_title[2] + " - " + list_company[2] + "\n" + list_href[2] + "\n\n" + list_title[3] + " - " + list_company[3] + "\n" + list_href[3] + '''\n
+''' + list_title[4] + " - " + list_company[4] + "\n" + list_href[4] + "\n\n" + list_title[5] + " - " + list_company[5] + "\n" + list_href[5] + '''\n
+''' + list_title[6] + " - " + list_company[6] + "\n" + list_href[6] + "\n\n" + list_title[7] + " - " + list_company[7] + "\n" + list_href[7] + '''\n
+''' + list_title[8] + " - " + list_company[8] + "\n" + list_href[8] + "\n\n" + list_title[9] + " - " + list_company[9] + "\n" + list_href[9] + '''\n
+''' + list_title[10] + " - " + list_company[10] + "\n" + list_href[10] + "\n\n" + list_title[11] + " - " + list_company[11] + "\n" + list_href[11] + '''\n
+''' + list_title[12] + " - " + list_company[12] + "\n" + list_href[12] + "\n\n" + list_title[13] + " - " + list_company[13] + "\n" + list_href[13] + '''\n
+''' + list_title[14] + " - " + list_company[14] + "\n" + list_href[14] + "\n\n" + list_title[15] + " - " + list_company[15] + "\n" + list_href[15] + '''\n
+''' + list_title[16] + " - " + list_company[16] + "\n" + list_href[16] + "\n\n" + list_title[17] + " - " + list_company[17] + "\n" + list_href[17] + '''\n
+''' + list_title[18] + " - " + list_company[18] + "\n" + list_href[18] + "\n\n" + list_title[19] + " - " + list_company[19] + "\n" + list_href[19] + "\n"
+  
+    return result
 
 
 
